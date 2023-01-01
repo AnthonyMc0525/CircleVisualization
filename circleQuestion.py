4c35ba34096c231416a5e8401f8f6ebfb30a1ef5
Anthony McMellen <anthonymcmellen@Anthonys-MacBook-Pro.local>
2023-01-01 16:16:20 -0500

    Fixes movement function

--- circleQuestion.py
+++ circleQuestion.py
@@ -7,45 +7,42 @@ WIDTH, HEIGHT = 900, 900
 pygame.display.set_caption("Orbit Visualization")
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-FPS = 5
+FPS = 60
 distance = 100
 orbit_color = (0, 0, 0)
 white = (255,255,255)
 black = (0, 0, 0)
 
-center_pos = [450, 450]
-
+center_pos_x = 450
+center_pos_y = 450
 orbit_current_pos_x = 450 + distance #x, y
 orbit_current_pos_y = 0 #x, y
-speed = 10 #speed of the dot in orbit
 orbit_origin_pos_x = orbit_current_pos_x 
 orbit_origin_pos_y = orbit_current_pos_y 
+theta = math.radians(45)
 
 
 # create and display objects on the screen
 def draw_window(orbit_current_pos):
-    pygame.draw.circle(WIN, white, center_pos , 10)
+    pygame.draw.circle(WIN, white, [ center_pos_x, center_pos_y ] , 10)
     pygame.draw.circle(WIN, white, [orbit_current_pos_x, orbit_current_pos_y], 10)
     pygame.display.update()
-
-def degreesToRadians(degree):
-    return degree * (math.pi/180)
-
 
 #function to move the orbiting item around the stationary item
 def moveY():
     global orbit_current_pos_x
     global orbit_current_pos_y
+    global center_pos_x
+    global center_pos_y
+    global theta
     #sin == y & cos == x
-    tan = math.tan(orbit_current_pos_y/orbit_current_pos_x)
-    theta = math.acos(degreesToRadians(tan))
-    x = math.cos(theta + degreesToRadians(speed)) * distance
-    y = math.sin(theta + degreesToRadians(speed)) * distance
-    orbit_current_pos_x = x + orbit_current_pos_x
-    orbit_current_pos_y = y + orbit_current_pos_y
-    print("x: " + str(orbit_current_pos_x))
-    print("y: " + str(orbit_current_pos_y))
-    print("-------------------------------------")
+    omega = 0.1 #Angular velocity
+    x = center_pos_x + distance * math.cos(theta)
+    y = center_pos_y - distance * math.sin(theta)
+    theta += omega # New angle, we add angular velocity
+    orbit_current_pos_x = x + distance * omega * math.cos(theta + math.pi / 2)
+    orbit_current_pos_y = y - distance * omega * math.sin(theta + math.pi / 2)
+
 
 # starting point and contains game loop
 def main():
