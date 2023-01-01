c52bb7a20b3b49d10866b6f3628fc5afc59caccf
Anthony McMellen <anthonymcmellen@Anthonys-MacBook-Pro.local>
2023-01-01 14:15:16 -0500

    Work in Progress

--- circleQuestion.py
+++ circleQuestion.py
@@ -7,7 +7,7 @@ WIDTH, HEIGHT = 900, 900
 pygame.display.set_caption("Orbit Visualization")
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-FPS = 1
+FPS = 5
 distance = 100
 orbit_color = (0, 0, 0)
 white = (255,255,255)
@@ -15,9 +15,9 @@ black = (0, 0, 0)
 
 center_pos = [450, 450]
 
-orbit_current_pos = [450 + distance, 450] #x, y
+orbit_current_pos = [450 + distance, 0] #x, y
 speed = 100 #speed of the dot in orbit
-orbit_origin_pos = orbit_current_pos
+orbit_origin_pos = orbit_current_pos 
 
 
 # create and display objects on the screen
@@ -30,20 +30,18 @@ def degreesToRadians(degree):
     return degree * (math.pi/180)
 
 
-#function to move the y item around the x item
+#function to move the orbiting item around the stationary item
 def moveY():
     global orbit_current_pos
     #sin == y & cos == x
-    #at pi, 0 theta == 0 as it is a line
     tan = math.tan(orbit_current_pos[1]/orbit_current_pos[0])
     theta = math.acos(degreesToRadians(tan))
     x = math.cos(theta + degreesToRadians(speed)) * distance 
-    print("x: " + str(x))
     y = math.sin(theta + degreesToRadians(speed)) * distance
-    print("y: " + str(y))
-    print("---------------------------------------")
     orbit_current_pos = [x + orbit_origin_pos[0], y + orbit_origin_pos[1]]
-       
+    print("x: " + str(orbit_current_pos[0]))
+    print("y: " + str(orbit_current_pos[1]))
+    print("-------------------------------------")
 
 # starting point and contains game loop
 def main():
